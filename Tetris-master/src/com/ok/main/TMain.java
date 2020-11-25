8ed9dea7acebba171cec30f3abd4d1044c2f0801
yin9931 <wjswjs6879@kakao.com>
2020-11-26 02:54:26 +0900

    update param #3

--- Tetris-master/src/com/ok/main/TMain.java
+++ Tetris-master/src/com/ok/main/TMain.java
@@ -1,13 +1,9 @@
 package com.ok.main;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -24,79 +20,87 @@ import com.ok.ai.TetrisRenderer;
 
 public class TMain extends JFrame {
 	
-	public static int SCREEN_WIDTH = 0;
-	public static int SCREEN_HEIGHT = 0;
+	public static int SCREEN_WIDTH = Main.SCREEN_WIDTH;
+	public static int SCREEN_HEIGHT = Main.SCREEN_HEIGHT;
 
 	//private Tetris uc;
+	//private TSetting setkey;
 	private TetrisRenderer ai;
-
+	private int key_size = 11;
 	
 	// ���� ���۸��� ���� ȭ�鿡 �̹����� ��� ���� Instance���̴�.
 	private Image screenImage;
 	private Graphics screenGraphic;
+	int image_x = 0;
+	int image_y = 0;
+	
+	// start button 
+	private ImageIcon startBasicImage = new ImageIcon(Main.class.getResource("../images/Start.png"));
+	private ImageIcon startEnteredImage = new ImageIcon(Main.class.getResource("../images/StartRed.png"));
+	private JButton startBtn;
+	int startBtn_w = SCREEN_WIDTH/5;
+	int startBtn_h = SCREEN_HEIGHT/7;
+	int startBtn_x = (SCREEN_WIDTH-startBtn_w)/2;
+	int startBtn_y = (int) (SCREEN_HEIGHT*4/13);
+	
+	// Setting button
+	private ImageIcon settingBasicImage = new ImageIcon(Main.class.getResource("../images/Setting.png"));
+	private ImageIcon settingEnteredImage = new ImageIcon(Main.class.getResource("../images/SettingRed.png"));
+	private JButton settingBtn;
+	int settingBtn_w = SCREEN_WIDTH/5;
+	int settingBtn_h = SCREEN_HEIGHT/7;
+	int settingBtn_x = (SCREEN_WIDTH-settingBtn_w)/2;
+	int settingBtn_y = (int) (SCREEN_HEIGHT*6/13);
+	
+	// Exit button
+	private ImageIcon exitBasicImage = new ImageIcon(Main.class.getResource("../images/Exit.png"));
+	private ImageIcon exitEnteredImage = new ImageIcon(Main.class.getResource("../images/ExitRed.png"));
+	private JButton exitBtn;
+	int exitBtn_w = SCREEN_WIDTH/5;
+	int exitBtn_h = SCREEN_HEIGHT/7;
+	int exitBtn_x = (SCREEN_WIDTH-exitBtn_w)/2;
+	int exitBtn_y = (int) (SCREEN_HEIGHT*8/13);
+	
+	// Ranking button
+	private ImageIcon rankingBasicImage = new ImageIcon(Main.class.getResource("../images/Ranking.png"));
+	private ImageIcon rankingEnteredImage = new ImageIcon(Main.class.getResource("../images/RankingRed.png"));
+	private JButton rankBtn;
+	int rankBtn_w = SCREEN_WIDTH/5;
+	int rankBtn_h = SCREEN_HEIGHT/7;
+	int rankBtn_x = (SCREEN_WIDTH-rankBtn_w)/2;
+	int rankBtn_y = (int) (SCREEN_HEIGHT*10/13);
 
-	// ��׶��� �̹��� ��ü ����
-	//private Image background = new ImageIcon(Main.class.getResource("../images/IntroBackground.png")).getImage();
-	private JLabel MainScreen = new JLabel(new ImageIcon(Main.class.getResource("../images/IntroBackground.png")));
-	
-	
-	// �޴��� ��ü ����
+	// menuBar
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+	int menuBar_x = 0;
+	int menuBar_y = 0;
+	int menuBar_w = SCREEN_WIDTH;
+	int menuBar_h = 30;
 
 	// �޴� �� ���� exit button ���� ��ü ����
 	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
 	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
 	private JButton exitButton = new JButton(exitButtonBasicImage);
-
-	// SINGLE button ���� ��ü ����
-	private Image singleImage = new ImageIcon(Main.class.getResource("../images/Start.png")).getImage();
-	private ImageIcon singleBasicImage = new ImageIcon(Main.class.getResource("../images/Start.png"));
-	private ImageIcon singleEnteredImage = new ImageIcon(Main.class.getResource("../images/StartRed.png"));
-	private JButton singleBtn = new JButton(singleBasicImage);
+	int exitButton_w = 30;
+	int exitButton_h = 30;
+	int exitButton_x = SCREEN_WIDTH-exitButton_w*3;
+	int exitButton_y = 0;
 	
-	//Ranking button
-	private Image rankingImage = new ImageIcon(Main.class.getResource("../images/Ranking.png")).getImage();
-	private ImageIcon rankingBasicImage = new ImageIcon(Main.class.getResource("../images/Ranking.png"));
-	private ImageIcon rankingEnteredImage = new ImageIcon(Main.class.getResource("../images/RankingRed.png"));
-	private JButton rankBtn = new JButton(rankingBasicImage);
-
-	// Normal button ���� ��ü ����
-	private Image normalImage = new ImageIcon(Main.class.getResource("../images/NormalBasic.png")).getImage();
-	private ImageIcon normalBasicImage = new ImageIcon(Main.class.getResource("../images/NormalBasic.png"));
-	private ImageIcon normalEnteredImage = new ImageIcon(Main.class.getResource("../images/NormalEntered.png"));
-	private JButton normalBtn = new JButton(normalBasicImage);
-
-	// Hard button ���� ��ü ����
-	private Image hardImage = new ImageIcon(Main.class.getResource("../images/HardBasic.png")).getImage();
-	private ImageIcon hardBasicImage = new ImageIcon(Main.class.getResource("../images/HardBasic.png"));
-	private ImageIcon hardEnteredImage = new ImageIcon(Main.class.getResource("../images/HardEntered.png"));
-	private JButton hardBtn = new JButton(hardBasicImage);
-
-	// Back button ���� ��ü ����
-	private Image backImage = new ImageIcon(Main.class.getResource("../images/BackBasic.png")).getImage();
-	private ImageIcon backBasicImage = new ImageIcon(Main.class.getResource("../images/BackBasic.png"));
-	private ImageIcon backEnteredImage = new ImageIcon(Main.class.getResource("../images/BackEntered.png"));
-	private JButton backBtn = new JButton(backBasicImage);
-
-	// Setting button ���� ��ü ����
-	private Image settingImage = new ImageIcon(Main.class.getResource("../images/Setting.png")).getImage();
-	private ImageIcon settingBasicImage = new ImageIcon(Main.class.getResource("../images/Setting.png"));
-	private ImageIcon settingEnteredImage = new ImageIcon(Main.class.getResource("../images/SettingRed.png"));
-	private JButton settingBtn = new JButton(settingBasicImage);
-
-	// Exit button ���� ��ü ����
-	private ImageIcon exitBasicImage = new ImageIcon(Main.class.getResource("../images/Exit.png"));
-	private ImageIcon exitEnteredImage = new ImageIcon(Main.class.getResource("../images/ExitRed.png"));
-	private JButton exitBtn = new JButton(exitBasicImage);
-
+	// MainScreen
+	private ImageIcon background = new ImageIcon(Main.class.getResource("../images/IntroBackground.png"));
+	private JLabel MainScreen;
+	int MainScreen_x = 0;
+	int MainScreen_y = 0;
+	int MainScreen_w = SCREEN_WIDTH;
+	int MainScreen_h = SCREEN_HEIGHT;
+	
 	private boolean isStartScreen = true;
-	
-	private boolean isSingleModeScreen = false;
+	private boolean isstartModeScreen = false;
 	private boolean isNormalModeScrren = false;
 	private boolean isHardModeScreen = false;
 	private boolean isSettingModeScreen = false;
 	
-	public int[] key_setting = new int[11];
+	public int[] key_setting = new int[key_size];
 	
 	// ���콺 �̺�Ʈ�� Ȱ���ϱ� ���� ���콺 x, y ��ǥ
 	private int mouseX, mouseY;
@@ -110,21 +114,132 @@ public class TMain extends JFrame {
 	}
 	
 	public TMain() {
-		
-		bgm_sound.play();
+		//bgm_sound.play();
 		setUndecorated(true); // �⺻ �޴��ٰ� ������ ����. -> ���ο� menuBar�� �ֱ� ���� �۾�
-		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-		setResizable(false); // ȭ�� ũ�� ���� �Ұ���
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setResizable(true); // ȭ�� ũ�� ���� �Ұ���
 		setLocationRelativeTo(null); // ȭ�� ���߾ӿ� �߰� ��.
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		setBackground(new Color(0, 0, 0, 255));
-	
+		setBackground(Color.black);
 		setLayout(null); // ȭ�鿡 ��ġ�Ǵ� ��ư�̳� label�� �� �ڸ� �״�� ���� ��.
+		
+		key_setting = SettingsDialog.DEFAULTS;
 
+		// Menu bar exit ��ư ���� ó��
+		startBtn = new JButton(startBasicImage);
+		startBtn.setBounds(startBtn_x, startBtn_y, startBtn_w, startBtn_h);
+		startBtn.setBorderPainted(false);
+		startBtn.setContentAreaFilled(false);
+		startBtn.setFocusPainted(false);
+		// exit Button �̺�Ʈ ó��
+		startBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				startBtn.setIcon(startEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
+				startBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				startBtn.setIcon(startBasicImage);
+				startBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// �̱� ��� ���� �̺�Ʈó�� �κ�
+				bgm_sound.stop();
+				normalModeScreen();
+			}
+		});
+		add(startBtn);
+
+		// Setting ��ư ���� ó��
+		settingBtn = new JButton(settingBasicImage);
+		settingBtn.setBounds(settingBtn_x, settingBtn_y, settingBtn_w, settingBtn_h);
+		settingBtn.setBorderPainted(false);
+		settingBtn.setContentAreaFilled(false);
+		settingBtn.setFocusPainted(false);
+		// Setting Button �̺�Ʈ ó��
+		settingBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				settingBtn.setIcon(settingEnteredImage); // ���콺�� Setting ��ư�� �ö󰡸� �̹����� �ٲ���.
+				settingBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				settingBtn.setIcon(settingBasicImage);
+				settingBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// Settingâ ȭ������ �Ѿ��
+				SettingsDialog.showDialog(TMain.this, key_setting);
+				//setkey = new TSetting(TMain.this);
+			}
+		});
+		add(settingBtn);
+
+		// Menu bar exit ��ư ���� ó��
+		exitBtn = new JButton(exitBasicImage);
+		exitBtn.setBounds(exitBtn_x, exitBtn_y, exitBtn_w, exitBtn_h);
+		exitBtn.setBorderPainted(false);
+		exitBtn.setContentAreaFilled(false);
+		exitBtn.setFocusPainted(false);
+		// exit Button �̺�Ʈ ó��
+		exitBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitBtn.setIcon(exitEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
+				exitBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitBtn.setIcon(exitBasicImage);
+				exitBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		add(exitBtn);
+		
+		//(rankBtn)
+		rankBtn = new JButton(rankingBasicImage);
+		rankBtn.setBounds(rankBtn_x, rankBtn_y, rankBtn_w, rankBtn_h);
+		rankBtn.setBorderPainted(false);
+		rankBtn.setContentAreaFilled(false);
+		rankBtn.setFocusPainted(false);
+		// exit Button �̺�Ʈ ó��
+		rankBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rankBtn.setIcon(rankingEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
+				rankBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rankBtn.setIcon(rankingBasicImage);
+				rankBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				ScoreFrame sf = new ScoreFrame();
+			}
+		});
+		add(rankBtn);
 		
 		// Menu bar exit ��ư ���� ó��
-		exitButton.setBounds(1820, 0, 30, 30);
+		exitButton.setBounds(exitButton_x, exitButton_y, exitButton_w, exitButton_h);
 		exitButton.setBorderPainted(false);
 		exitButton.setContentAreaFilled(false);
 		exitButton.setFocusPainted(false);
@@ -150,7 +265,7 @@ public class TMain extends JFrame {
 		add(exitButton);
 
 		// �޴��� �̺�Ʈ
-		menuBar.setBounds(0, 0, 1920, 30);
+		menuBar.setBounds(menuBar_x, menuBar_y, menuBar_w, menuBar_h);
 		menuBar.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) { // ���콺 Ŭ�� �� x,y ��ǥ�� ����.
 				mouseX = e.getX();
@@ -165,188 +280,20 @@ public class TMain extends JFrame {
 			}
 		});
 		add(menuBar);
-
-		// Single ��ư ���� ó��
-		singleBtn.setBounds(755, 350, 410, 110);
-		singleBtn.setBorderPainted(false);
-		singleBtn.setContentAreaFilled(false);
-		singleBtn.setFocusPainted(false);
-		// exit Button �̺�Ʈ ó��
-		singleBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				singleBtn.setIcon(singleEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
-				singleBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				singleBtn.setIcon(singleBasicImage);
-				singleBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// �̱� ��� ���� �̺�Ʈó�� �κ�
-				bgm_sound.stop();
-				normalModeScreen();
-			}
-		});
-		add(singleBtn);
-		//(rankBtn)
-		rankBtn.setBounds(755, 800, 410, 110);
-		rankBtn.setBorderPainted(false);
-		rankBtn.setContentAreaFilled(false);
-		rankBtn.setFocusPainted(false);
-		// exit Button �̺�Ʈ ó��
-		rankBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				rankBtn.setIcon(rankingEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
-				rankBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				rankBtn.setIcon(rankingBasicImage);
-				rankBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				ScoreFrame sf = new ScoreFrame();
-			}
-		});
-		add(rankBtn);
-
-		// Hard ��ư ���� ó��
-		hardBtn.setBounds(130, 340, 400, 100);
-		hardBtn.setBorderPainted(false);
-		hardBtn.setContentAreaFilled(false);
-		hardBtn.setFocusPainted(false);
-		hardBtn.setVisible(false);
-		// hard Button �̺�Ʈ ó��
-		hardBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				hardBtn.setIcon(hardEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
-				hardBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				hardBtn.setIcon(hardBasicImage);
-				hardBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// �ϵ� ��� ���� �̺�Ʈó�� �κ�
-			}
-		});
-		add(hardBtn);
-
-		// Back ��ư ���� ó��
-		backBtn.setBounds(130, 470, 400, 100);
-		backBtn.setBorderPainted(false);
-		backBtn.setContentAreaFilled(false);
-		backBtn.setFocusPainted(false);
-		backBtn.setVisible(false);
-		// exit Button �̺�Ʈ ó��
-		backBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				backBtn.setIcon(backEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
-				backBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				backBtn.setIcon(backBasicImage);
-				backBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// �ڷ� ���� �̺�Ʈ ó���κ�
-			}
-		});
-		add(backBtn);
-
-		// Setting ��ư ���� ó��
-		settingBtn.setBounds(755, 500, 410, 110);
-		settingBtn.setBorderPainted(false);
-		settingBtn.setContentAreaFilled(false);
-		settingBtn.setFocusPainted(false);
-		// Setting Button �̺�Ʈ ó��
-		settingBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				settingBtn.setIcon(settingEnteredImage); // ���콺�� Setting ��ư�� �ö󰡸� �̹����� �ٲ���.
-				settingBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				settingBtn.setIcon(settingBasicImage);
-				settingBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// Settingâ ȭ������ �Ѿ��
-				SettingsDialog.showDialog(TMain.this, key_setting);
-			}
-		});
-		add(settingBtn);
-
-		// exit ��ư ���� ó��
-		exitBtn.setBounds(750, 650, 410, 110);
-		exitBtn.setBorderPainted(false);
-		exitBtn.setContentAreaFilled(false);
-		exitBtn.setFocusPainted(false);
-		// exit Button �̺�Ʈ ó��
-		exitBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				exitBtn.setIcon(exitEnteredImage); // ���콺�� exit ��ư�� �ö󰡸� �̹����� �ٲ���.
-				exitBtn.setCursor(new Cursor(Cursor.HAND_CURSOR)); // ���콺�� �ö󰡸� �հ��� ������ιٲ�
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				exitBtn.setIcon(exitBasicImage);
-				exitBtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // ���콺�� ���� �ٽ� ����Ʈ ������� �ٲ�
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				System.exit(0);
-			}
-		});
-		add(exitBtn);
 		
-		MainScreen.setBounds(0,0, 1920, 1080);
+		// Main Screen
+		MainScreen = new JLabel(background);
+		MainScreen.setBounds(MainScreen_x, MainScreen_y, MainScreen_w, MainScreen_h);
 		MainScreen.setVisible(true);
 		add(MainScreen);
-		
-		key_setting = SettingsDialog.DEFAULTS;
 	}
-	
 
 	public void paint(Graphics g) {
 		// 1280X720��ŭ�� �̹����� ��������� screenImage�� �־���.
-		//screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-		//screenGraphic = screenImage.getGraphics(); // screenImage�� �̿��� �׷����� ����.
-		//screenDraw(screenGraphic); // ��ũ���� �׷����� �׷���.
-		//screenGraphic.drawImage(background, 0, 0, null);
-		//g.drawImage(background, 0, 0, null);
 		paintComponents(g);
 		this.revalidate();
 		this.repaint();
-		//Toolkit.getDefaultToolkit().sync();
-		g.drawImage(screenImage, 0, 0, null);
-		//g.drawImage(background, 0, 0, null);
+		g.drawImage(screenImage, image_x, image_y, null);
 		//Toolkit.getDefaultToolkit().sync();
 	}
 
